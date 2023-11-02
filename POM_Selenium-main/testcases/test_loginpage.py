3c3ff16bd1ba2f70e228d6060b3d45e2336b354e
Fathima-Kubra <148780687+Fathima-Kubra@users.noreply.github.com>
2023-11-02 16:33:04 +0530

    Update test_loginpage.py

--- POM_Selenium-main/testcases/test_loginpage.py
+++ POM_Selenium-main/testcases/test_loginpage.py
@@ -15,6 +15,7 @@ class Test_Login(BaseTest):
     def test_login1(self, driver):
         objlogin = Login(self, driver)
         objlogin.login("Admin","admin123")
+        print('demo went great')
 
     # def test_login_title(self):
     #     obj = Login()
